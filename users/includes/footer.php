6f6f3c6980752bff6aaa81eeb3e42a3f5a170f32
Ayush <ayush.chakladar@gmail.com>
2021-11-20 01:13:31 +0530

    first commit

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              999999999 | For any queries contact Quality Assurance Team Lead
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
